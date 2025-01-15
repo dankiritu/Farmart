0c2ac4570dcbff45ac126267f71db453b561ce0f
thegr8ivan <ivan.kamotho@student.moringaschoool.com>
2025-01-15 23:10:45 +0300

    Initial commit for Farmart backend

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://farmart_user:yourpassword@localhost/farmart'
+db = SQLAlchemy(app)
 
-@app.route('/')
-def home():
-    return "Welcome to Farmart!"
+# Define your models (e.g., Animal, User)
+class Animal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    type = db.Column(db.String(80), nullable=False)
 
-if __name__ == "__main__":
+@app.route('/animals', methods=['GET'])
+def get_animals():
+    animals = Animal.query.all()
+    return jsonify([animal.name for animal in animals])
+
+if __name__ == '__main__':
     app.run(debug=True)
-
---
+++ config.py
@@ -0,0 +1,1 @@
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://farmart_user:yourpassword@localhost/farmart'
