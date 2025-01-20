0e6005668e0aeac3e3a2fe8484ddfb70da4dc6f3
thegr8ivan <ivan.kamotho@student.moringaschoool.com>
2025-01-20 12:14:28 +0300

    initial commit backend

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://farmart_user:yourpassword@localhost/farmart'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://farmart_user:password@localhost/farmart_db'
 db = SQLAlchemy(app)
 
-# Define your models (e.g., Animal, User)
-class Animal(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    type = db.Column(db.String(80), nullable=False)
+@app.route('/')
+def home():
+    return jsonify({"message": "Welcome to Farmart Backend!"})
 
-@app.route('/animals', methods=['GET'])
-def get_animals():
-    animals = Animal.query.all()
-    return jsonify([animal.name for animal in animals])
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
